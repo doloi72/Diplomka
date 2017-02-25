86be69c44eb8fc5c946e0162c36b7d8bc81b7c7a
Martin Dolinsky <martin.dolinsky72@gmail.com>
2017-02-25 19:23:08 +0100

    Nerozumiem preco nefunguje geolokacia pri netrowk_provider

--- app/src/main/java/com/example/doloi/diplomka/ui/DetailActivity.java
+++ app/src/main/java/com/example/doloi/diplomka/ui/DetailActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.doloi.diplomka.R;
@@ -38,5 +39,10 @@ public class DetailActivity extends AppCompatActivity {
             mTitle.setText(title);
             mSubtitle.setText(subtitle);
         }
+    }
+
+    public void jumpToLocation(View view){
+        Intent intent = new Intent(this, GeoActivity.class);
+        startActivity(intent);
     }
 }
