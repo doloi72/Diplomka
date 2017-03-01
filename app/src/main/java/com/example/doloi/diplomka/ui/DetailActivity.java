fdaffdcad1648cb21a99f42fe5f8ff460f060688
Martin Dolinsky <martin.dolinsky72@gmail.com>
2017-03-01 20:07:12 +0100

    Pridanie Activity pre lokalizovanie polohy na mape, zmenu terenu a zakladne informacie o danom bode...

--- app/src/main/java/com/example/doloi/diplomka/ui/DetailActivity.java
+++ app/src/main/java/com/example/doloi/diplomka/ui/DetailActivity.java
@@ -24,6 +24,7 @@ public class DetailActivity extends AppCompatActivity {
         return i;
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
